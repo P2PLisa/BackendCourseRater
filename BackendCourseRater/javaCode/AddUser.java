366b6a8eef3784c32faff4b8f60415c9a1a469e5
Lisa Campbell <lisasoupcampbell@gmail.com>
2016-01-16 23:47:50 -0500

    Added AWS lib

---
+++ BackendCourseRater/javaCode/AddUser.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.model.AttributeDefinition;
+import com.amazonaws.services.dynamodbv2.model.KeySchemaElement;
+import com.amazonaws.services.dynamodbv2.model.KeyType;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import com.amazonaws.services.dynamodbv2.model.ScalarAttributeType;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.ItemCollection;
+import com.amazonaws.services.dynamodbv2.document.QueryOutcome;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.QuerySpec;
+import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
+
+public class AddUser {
+    public static void main(String [] args) {
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient()
+                .withEndpoint("http://localhost:8000");
+
+        DynamoDB dynamoDB = new DynamoDB(client);
+
+        Table table = dynamoDB.getTable("BasicUserInfo");
+    }
+}
